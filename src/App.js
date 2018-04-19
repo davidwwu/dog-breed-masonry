fdfb54d90d36406a286fb8f3a559bd7329aae581
David Wu <david@wudavid.com>
2018-04-19 01:44:29 -0700

    make the masonry full screen and responsive to window sizes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 
 import axios from 'axios';
-// import Masonry from 'react-masonry-component';
 import { AutoSizer, CellMeasurer, CellMeasurerCache, createMasonryCellPositioner, Masonry, WindowScroller } from 'react-virtualized';
 import 'react-virtualized/styles.css';
 
@@ -11,21 +10,28 @@ class App extends Component {
   constructor(props) {
     super(props);
 
+    this.list = {};           // store initial breed list
+    this.columnCount = 0;
+    this.columnWidth = 400;
+    this.gutterSize = 10;
+
     this.state = {
-      list: {},
-      breedsList: [],
-      breedsListReady: false,
-      imgReady: false
+      breeds_ajax: [],        // list of all image requests (flattened out from the original list tree) 
+      breedsList: [],         // store all image src's
+      breedsListReady: false  // indicate that the list is ready
     }
 
+    // Initial request to get the breeds list
     axios.get('https://dog.ceo/api/breeds/list/all')
     .then((res) => {
-      this.setState({ list: {...res.data.message} })
+      this.list = {...res.data.message};
 
+      // Flatten out the list tree
       let temp_breeds_list = [];
-      for (let breed in this.state.list) {
-        if (this.state.list[breed].length > 0) {
-          this.state.list[breed].forEach(subBreed => {
+      
+      for (let breed in this.list) {
+        if (this.list[breed].length > 0) {
+          this.list[breed].forEach(subBreed => {
             temp_breeds_list.push(axios.get(`https://dog.ceo/api/breed/${breed}/${subBreed}/images`));
           });
         } else {
@@ -33,14 +39,14 @@ class App extends Component {
         }
       }
 
-      this.setState({ 
-        breeds: temp_breeds_list,
-        breedsListReady: true
-      });
+      this.setState({ breeds_ajax: temp_breeds_list });
 
+      // Since in our breeds list we only have requests to get
+      // images from each breed, we will then make calls to all
+      // of the breeds so we can have a list of image src's
       let temp_img_list = [];
      
-      axios.all(this.state.breeds)
+      axios.all(this.state.breeds_ajax)
       .then((res) => {
         res.forEach(response => {
           temp_img_list = temp_img_list.concat(response.data.message);
@@ -48,8 +54,10 @@ class App extends Component {
 
         this.setState({
           breedsList: temp_img_list,
-          imgReady: true
+          breedsListReady: true
         });
+      }).catch((err) => {
+        console.error(err);
       });
     })
     .catch((err) => {
@@ -62,38 +70,110 @@ class App extends Component {
       defaultWidth: 200,
       fixedWidth: true
     })
+  }
 
-    // Our masonry layout will use 3 columns with a 10px gutter between
-    this.cellPositioner = createMasonryCellPositioner({
-      cellMeasurerCache: this.cache,
-      columnCount: 3,
-      columnWidth: 200,
-      spacer: 10
-    })
+  // Initialize the cell position
+  initCellPositioner = () => {
+    if (typeof this._cellPositioner === 'undefined') {
 
-    this.cellRenderer = ({ index, key, parent, style }) => {
-      const datum = this.state.breedsList[index]
-    
-      return (
-        <CellMeasurer
-          cache={this.cache}
-          index={index}
-          key={key}
-          parent={parent}
-        >
-          <div style={style}>
-            <img
-              src={datum}
-              style={{
-                width: 200,
-                height: 300
-              }}
-            />
-          </div>
-        </CellMeasurer>
-      )
+      this._cellPositioner = createMasonryCellPositioner({
+        cellMeasurerCache: this.cache,
+        columnCount: this.columnCount,
+        columnWidth: this.columnWidth,
+        spacer: this.gutterSize,
+      });
     }
+  }
 
+  // Handle window resize event
+  onResize = ({ width }) => {
+    this._windowWidth = width;
+
+    this.calculateColumnCount();
+    this.resetCellPositioner();
+    this._masonry.recomputeCellPositions();
+  }
+
+  // Render resizable component containing the masonry component
+  renderAutoSizer = ({ height, scrollTop }) => {
+    this._windowHeight = height;
+    this._scrollTop = scrollTop;
+
+    return (
+      <AutoSizer
+        disableHeight
+        height={height}
+        onResize={this.onResize}
+        scrollTop={scrollTop}>
+        {this.renderMasonry}
+      </AutoSizer>
+    );
+  }
+
+  // Calculate column nubmer based on window size
+  calculateColumnCount = () => {
+    this.columnCount = Math.floor(this._windowWidth / (this.columnWidth + this.gutterSize));
+  }
+
+  // reset cell positioner
+  resetCellPositioner = () => {
+    this._cellPositioner.reset({
+      columnCount: this.columnCount,
+      columnWidth: this.columnWidth,
+      spacer: this.gutterSize,
+    });
+  }
+
+  // Render the masonry component
+  renderMasonry = ({ height, width, onChildScroll}) => {
+    this._windowWidth = width;
+
+    this.calculateColumnCount();
+    this.initCellPositioner();
+
+    return (
+      <Masonry
+        autoHeight
+        cellCount={this.state.breedsList.length}
+        cellMeasurerCache={this.cache}
+        cellPositioner={this._cellPositioner}
+        cellRenderer={this.cellRenderer}
+        onScroll={onChildScroll}
+        scrollTop={this._scrollTop}
+        ref={this.setMasonryRef}
+        height={this._windowHeight}
+        width={this._windowWidth}
+      />
+    );
+  }
+
+  // Render each cell/image
+  cellRenderer = ({ index, key, parent, style }) => {
+    const img_src = this.state.breedsList[index];
+  
+    return (
+      <CellMeasurer
+        cache={this.cache}
+        index={index}
+        key={key}
+        parent={parent}
+      >
+        <div style={style}>
+          <img
+            src={img_src}
+            style={{
+              width: this.columnWidth,
+              height: 200
+            }}
+          />
+        </div>
+      </CellMeasurer>
+    )
+  }
+
+  // set the masonry ref to _masonry so its methods are available 
+  setMasonryRef = (ref) => {
+    this._masonry = ref;
   }
 
   render() {
@@ -108,15 +188,10 @@ class App extends Component {
         <div className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload. <br />
 
-          {this.state.imgReady ?
-            <Masonry
-              cellCount={this.state.breedsList.length}
-              cellMeasurerCache={this.cache}
-              cellPositioner={this.cellPositioner}
-              cellRenderer={this.cellRenderer}
-              height={600}
-              width={800}
-            />
+          {this.state.breedsListReady ?
+            <WindowScroller>
+              {this.renderAutoSizer}
+            </WindowScroller>
             : "loading..."}
         </div>
       </div>
